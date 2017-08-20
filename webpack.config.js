c61eef7047b2dbf275c8fe835e3ad47f1071b0bb
lamtan@ <lamtan93@gmail.com>
2017-08-20 21:54:53 +0200

    Etape 1 - Mise en place Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+/*var UglifyJSPlugin = require('uglifyjs-webpack-plugin');*/
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//console.log( path.resolve(__dirname, "build"));
+
+module.exports = {
+// définition des points d'entrée
+// il est possible de définir plusieurs points d'entrée
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	plugins: [
+	/*new UglifyJSPlugin(),*/
+	new HtmlWebpackPlugin()
+	]
+};
